13311e6fb77b13ac8deadc53e06dd01ca468b1f3
Mahesh Kumar <127003692+maheshrajbhar90@users.noreply.github.com>
2026-02-19 18:56:17 +0530

    Update __init__.py

--- EdgeQuantAI-main/EdgeQuantAI/__init__.py
+++ EdgeQuantAI-main/EdgeQuantAI/__init__.py
@@ -8,7 +8,7 @@ Version: 0.0.0.1
 """
 
 # Import the main classes and functions that should be accessible when the package is imported
-from .stock_analyzer import StockTechnicalAnalyzer  # Assuming your main class is in a file named 'stock_analyzer.py'
+from .EdgeQuantAI import StockTechnicalAnalyzer  # Assuming your main class is in a file named 'stock_analyzer.py'
 from .fundamental_analyzer import FundamentalAnalyser
 
 
